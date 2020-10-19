b44d3e9e1cb725fd5af5d23c9e93c3f34ee3acbf
e1b18003 <e1b18003@oit.ac.jp>
2020-10-19 18:56:37 +0900

    ログインユーザ表示

--- src/main/java/oit/is/z0744/kaizi/janken/controller/Lec02Controller.java
+++ src/main/java/oit/is/z0744/kaizi/janken/controller/Lec02Controller.java
@@ -1,5 +1,8 @@
 package oit.is.z0744.kaizi.janken.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,18 +10,33 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import oit.is.z0744.kaizi.janken.model.Entry;
+
 @Controller
 public class Lec02Controller {
+
+  @Autowired
+  private Entry room;
 
   /**
    *
    * @return
    */
+
+   /**
+   *
+   * @param model Thymeleafにわたすデータを保持するオブジェクト
+   * @param prin  ログインユーザ情報が保持されるオブジェクト
+   * @return
+   */
   @GetMapping("/lec02/step1")
-  public String lec02() {
+  public String lec02(Principal prin, ModelMap model) {
+    String loginUser = prin.getName();
+    this.room.addUser(loginUser);
+    model.addAttribute("room", this.room);
+
     return "lec02.html";
   }
-
   /**
    * @param namae
    * @return
@@ -54,5 +72,14 @@ public class Lec02Controller {
 
     return "lec02.html";
   }
+
+  /**
+   *
+   * @param model Thymeleafにわたすデータを保持するオブジェクト
+   * @param prin  ログインユーザ情報が保持されるオブジェクト
+   * @return
+   */
+
+
 
 }
