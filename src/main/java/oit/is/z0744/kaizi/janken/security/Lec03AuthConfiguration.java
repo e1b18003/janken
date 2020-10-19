0c28dfe702ca7830ce855d6bca64ddbbc7903f76
e1b18003 <e1b18003@oit.ac.jp>
2020-10-19 16:33:33 +0900

    ログイン認可処理追加

--- src/main/java/oit/is/z0744/kaizi/janken/security/Lec03AuthConfiguration.java
+++ src/main/java/oit/is/z0744/kaizi/janken/security/Lec03AuthConfiguration.java
@@ -14,11 +14,22 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class Lec03AuthConfiguration extends WebSecurityConfigurerAdapter {
 
   @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication().withUser("user1").password(passwordEncoder().encode("pAssw0rd")).roles("USER");
+    auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder().encode("pAssw0rd")).roles("USER");
+  }
+
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Override
   protected void configure(HttpSecurity http) throws Exception {
 
     http.formLogin();
 
-    http.authorizeRequests().antMatchers("/lec02/step1").authenticated();
+    http.authorizeRequests().antMatchers("/lec02/**").authenticated();
 
     http.logout().logoutSuccessUrl("/");
   }
